89436b36a1f1a276e8078ff98658cef257f877a0
drakanis <brandon@thetechpirate.net>
2016-11-22 18:13:57 -0700

    Updated with comments

--- MPG_Calculator/MPG_Calculator/Form1.cs
+++ MPG_Calculator/MPG_Calculator/Form1.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/*  The purpose of this program is to calculate Miles Per Gallon and Dollars Per Mile based on user input.
+ *  
+ *  This program is designed by Brandon Vader
+ *  11-22-2016
+ *  
+ *  github.com/drakanis/MPG_Calculator 
+ */
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -19,6 +27,8 @@ namespace MPG_Calculator
 
         private void btnMPG_Click(object sender, EventArgs e)
         {
+            //Click MPG button to calculate MPG
+
             double dMiles, dGallons, dMPG;
 
             dMiles = Convert.ToDouble(txtMiles.Text);
@@ -27,11 +37,13 @@ namespace MPG_Calculator
 
             dMPG = dMiles / dGallons;
 
-            lblMPG.Text = dMPG.ToString("##.##");
+            lblMPG.Text = dMPG.ToString("###.##"); //Limiting to hundredths as further accuracy is not required
         }
 
         private void btnDPM_Click(object sender, EventArgs e)
         {
+            //Click $/Mile button to calculate the Dollars per Mile cost (uasually cents per mile, but makes people feel better to see a 0 dollar)
+
             double dMiles, dGallons, dDPG, dDPM;
 
             dMiles = Convert.ToDouble(txtMiles.Text);
@@ -40,11 +52,13 @@ namespace MPG_Calculator
 
             dDPM = (dGallons * dDPG) / dMiles;
 
-            lblDPM.Text = dDPM.ToString("c");
+            lblDPM.Text = dDPM.ToString("c"); //Displays as currency (ie. dollar sign, and cents value)
         }
 
         private void btnClear_Click(object sender, EventArgs e)
         {
+            //Click Clear to clear all output and input so that you can use program again
+
             txtDPG.Text = "";
             txtGallons.Text = "";
             txtMiles.Text = "";
